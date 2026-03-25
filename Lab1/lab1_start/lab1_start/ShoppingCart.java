21a0f196b874efad81b780f3921a58524ed63990
executem <isak.o.soderlind@gmail.com>
2026-03-25 09:22:46 +0100

    lab1 part0

--- Lab1/lab1_start/lab1_start/ShoppingCart.java
+++ Lab1/lab1_start/lab1_start/ShoppingCart.java
@@ -20,14 +20,29 @@ public class ShoppingCart {
 
         print(wallet, pocket);
         String product = scan(scanner);
-
+        
         while(!product.equals("quit")) {
+            try {
+                Integer price = Store.getProductPrice(product);
+                if(wallet.getBalance() < price){
+                    System.out.println("Balance not sufficient to buy product: " + product);
+                    break;
+
+                } else {
+                    wallet.setBalance(wallet.getBalance() - price);
+                    pocket.addProduct(product);
+                    System.out.println("You bought " + product + " for " + price + " credits.");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("Product " + product + " is not in store");
+            }
             /* TODO:
                - check if the amount of credits is enough, if not stop the execution.
                - otherwise, withdraw the price of the product from the wallet.
                - add the name of the product to the pocket file.
                - print the new balance.
             */
+           
 
             // Just to print everything again...
             print(wallet, pocket);
